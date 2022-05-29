6c8a88e463a99f58a5f804a98359b5138cbdda07
Robson Souza <robsonjs@ciandt.com>
2022-05-29 17:28:32 -0300

    Bootstrap

--- test/acceptance/pages/google.page.js
+++ test/acceptance/pages/google.page.js
@@ -6,6 +6,8 @@ const navigate = async () => {
 }
 
 const fillSearchTextAndEnter = async (searchText) => {
+    const content = await page.textContent('html');
+    console.log(content);
     const searchInput = await page.locator(selector.SEARCH_INPUT);
     await searchInput.type(searchText); // pode ser usado o type também para simular melhor a interação com usuário
     await searchInput.press("Enter");  

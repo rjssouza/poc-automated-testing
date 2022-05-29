df692a5f2f2b6919e9f4b493bd109b76c31ae6ed
Robson Souza <robsonjs@ciandt.com>
2022-05-29 17:23:38 -0300

    Bootstrap

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import cucumber from '@cucumber/cucumber/lib/cli/run.js'
 import { config } from 'dotenv'
 config();
 
-console.log(JSON.stringify(process.env));
-
 global.testing = { env: process.env };
 
 cucumber();
